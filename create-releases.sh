5f0c910d30faa15d1210f647b608eb57e18116a1
dub-flow <89262088+dub-flow@users.noreply.github.com>
2024-03-18 19:35:32 +0100

    Added script to create releases

---
+++ create-releases.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# get the current version of the tool from `./VERSION`
+VERSION=$(cat VERSION)
+
+FLAGS="-X main.AppVersion=$VERSION -s -w"
+
+rm -rf releases
+mkdir -p releases
+
+# build for Windows
+GOOS=windows GOARCH=amd64 go build -ldflags="$FLAGS" -trimpath
+mv subsnipe.exe releases/subsnipe-windows-amd64.exe
+
+# build for M1 Macs (arm64)
+GOOS=darwin GOARCH=arm64 go build -ldflags="$FLAGS" -trimpath
+mv subsnipe releases/subsnipe-mac-arm64
+
+# build for Intel Macs (amd64)
+GOOS=darwin GOARCH=amd64 go build -ldflags="$FLAGS" -trimpath
+mv subsnipe releases/subsnipe-mac-amd64
+
+# build for x64 Linux (amd64)
+GOOS=linux GOARCH=amd64 go build -ldflags="$FLAGS" -trimpath
+mv subsnipe releases/subsnipe-linux-amd64
