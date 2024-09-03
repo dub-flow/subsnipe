67c74ff9993de637906e8961877cfc4a6d88a1e2
dub-flow <89262088+dub-flow@users.noreply.github.com>
2024-09-03 14:56:25 +0200

    Updated the docker-push.sh file to create its own buildx profile

--- docker-push.sh
+++ docker-push.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env sh
 
-# get the current version of the tool from `./VERSION`
+# Get the current version of the tool from `./VERSION`
 VERSION=$(cat VERSION)
 
+# Create a new Buildx builder with docker-container driver
+BUILDER_NAME=mybuilder
+
+# Remove any existing builder with the same name
+docker buildx rm $BUILDER_NAME 2>/dev/null || true
+
+# Create a new builder
+docker buildx create --name $BUILDER_NAME --driver docker-container --use
+
+# Build and push the Docker image
 docker buildx build --platform linux/amd64,linux/arm64 --push . -t fw10/subsnipe:$VERSION -t fw10/subsnipe:latest
